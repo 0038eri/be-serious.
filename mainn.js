572956062f9ec734f22bda5ae4430755baf593ac
Eri Osawa <osawaeri@users.noreply.github.com>
2018-08-08 01:36:33 +0900

    30

--- mainn.js
+++ mainn.js
@@ -1,88 +1,88 @@
 $(function () {
 
-    window.onload = function () {
-        section01Anm();
-    }
+            window.onload = function () {
+                section01Anm();
+            }
 
-    $.scrollify({
-        section: ".content",
-        scrollSpeed: 1600,
-        scrollbars: false,
-        updateHash: false
-    });
+            $.scrollify({
+                section: ".content",
+                scrollSpeed: 1600,
+                scrollbars: false,
+                updateHash: false
+            });
 
-    // セクション1つめアニメーション
-
-    function beSerious() {
-        $('#serious h1').css({
-            "opacity": "1.0",
-            "transform": "translateY(0%)"
-        });
-    }
-
-    function honkiNiNarou() {
-        $('#serious .h2').css({
-            "opacity": "1.0",
-            "transform": "translateY(0%)"
-        });
-    }
-
-    function section01Anm() {
-        beSerious();
-        setTimeout(honkiNiNarou, 200);
-    }
-
-    // セクション2つめのアニメーション
-
-    function message() {
-        $('#message h1').css({
-            "opacity": "1.0",
-            "transform": "translateY(0%)"
-        });
-    }
-
-    function paragraph01() {
-        $('#message .p01').css({
-            "opacity": "1.0",
-            "transform": "translateY(0%)"
-        });
-    }
-
-    function paragraph02() {
-        $('#message .p02').css({
-            "opacity": "1.0",
-            "transform": "translateY(0%)"
-        });
-    }
-
-    function paragraph03() {
-        $('#message .p03').css({
-            "opacity": "1.0",
-            "transform": "translateY(0%)"
-        });
-    }
-
-    function paragraph04() {
-        $('#message .p04').css({
-            "opacity": "1.0",
-            "transform": "translateY(0%)"
-        });
-    }
-
-    function paragraph05() {
-        $('#message .p05').css({
-            "opacity": "1.0",
-            "transform": "translateY(0%)"
-        });
-    }
-
-    function section02Anm() {
-        message();
-        setTimeout(paragraph01, 200);
-        setTimeout(paragraph02, 400);
-        setTimeout(paragraph03, 600);
-        setTimeout(paragraph04, 800);
-        setTimeout(paragraph05, 1000);
-    }
-
-});
+                // セクション1つめアニメーション
+            
+                function beSerious() {
+                    $('#serious h1').css({
+                        "opacity": "1.0",
+                        "transform": "translateY(0%)"
+                    });
+                }
+            
+                function honkiNiNarou() {
+                    $('#serious .h2').css({
+                        "opacity": "1.0",
+                        "transform": "translateY(0%)"
+                    });
+                }
+            
+                function section01Anm() {
+                    beSerious();
+                    setTimeout(honkiNiNarou, 200);
+                }
+            
+                // セクション2つめのアニメーション
+            
+                function message() {
+                    $('#message h1').css({
+                        "opacity": "1.0",
+                        "transform": "translateY(0%)"
+                    });
+                }
+            
+                function paragraph01() {
+                    $('#message .p01').css({
+                        "opacity": "1.0",
+                        "transform": "translateY(0%)"
+                    });
+                }
+            
+                function paragraph02() {
+                    $('#message .p02').css({
+                        "opacity": "1.0",
+                        "transform": "translateY(0%)"
+                    });
+                }
+            
+                function paragraph03() {
+                    $('#message .p03').css({
+                        "opacity": "1.0",
+                        "transform": "translateY(0%)"
+                    });
+                }
+            
+                function paragraph04() {
+                    $('#message .p04').css({
+                        "opacity": "1.0",
+                        "transform": "translateY(0%)"
+                    });
+                }
+            
+                function paragraph05() {
+                    $('#message .p05').css({
+                        "opacity": "1.0",
+                        "transform": "translateY(0%)"
+                    });
+                }
+            
+                function section02Anm() {
+                    message();
+                    setTimeout(paragraph01, 200);
+                    setTimeout(paragraph02, 400);
+                    setTimeout(paragraph03, 600);
+                    setTimeout(paragraph04, 800);
+                    setTimeout(paragraph05, 1000);
+                }
+            
+            });
