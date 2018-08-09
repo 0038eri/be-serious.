f3a373b14162b234017cd63030709acb67923088
Eri Osawa <osawaeri@users.noreply.github.com>
2018-08-09 23:57:50 +0900

    33

--- mainn.js
+++ mainn.js
@@ -1,13 +1,5 @@
 $(function () {
 
-    $('.effect').css({
-        "opacity": "0"
-    });
-
-    //    window.onload = function () {
-    //        section01Anm();
-    //    }
-
     $.scrollify({
         section: ".content",
         scrollSpeed: 1600,
@@ -15,53 +7,13 @@ $(function () {
         updateHash: false
     });
 
-    //    var id;
-    //    $('.content').waypoint(function (direction) {
-    //        var activePoint = $(this.element);
-    //        if (direction === 'down') {
-    //            activePoint.addClass('active');
-    //            id = $(activePoint) a.attr('id');
-    //            switch (id) {
-    //                case "serious":
-    //                    section01Anm();
-    //                    break;
-    //                case "message":
-    //                    section02Anm();
-    //                    break;
-    //                default:
-    //                    break;
-    //            }
-    //        } else { //scroll up
-    //            activePoint.removeClass('active');
-    //        }
-    //    }, {
-    //        offset: '0%'
-    //    });
-
-    // セクション1つめアニメーション
-
-    function section01Anm() {
-        $('#serious h1').css({
-            "opacity": "1.0",
-            "transform": "translateY(0%)"
-        });
-        $('#serious .h2').css({
-            "opacity": "1.0",
-            "transform": "translateY(0%)"
-        });
-    }
-
-    // セクション2つめのアニメーション
-
-    function section02Anm() {
-        $('#message h1').css({
-            "opacity": "1.0",
-            "transform": "translateY(0%)"
-        });
-        $('#message h3').css({
-            "opacity": "1.0",
-            "transform": "translateY(0%)"
-        });
-    }
-
-}); 
+    // フェードインしながら上へスライド     
+    $('.inviewfadeInUp').on('inview', function (event, isInView, visiblePartX, visiblePartY) {
+        if (isInView) {
+            $(this).stop().addClass('fadeInUp');
+        } else {
+            $(this).stop().removeClass('fadeInUp');
+        }
+    });
+    
+});
