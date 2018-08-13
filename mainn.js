d9e18cfd950bcdea73ea22a1a275090a3da8fc95
Eri Osawa <osawaeri@users.noreply.github.com>
2018-08-14 01:54:20 +0900

    38

--- mainn.js
+++ mainn.js
@@ -1,26 +1,28 @@
 $(function () {
 
-        $.scrollify({
-            section: ".content",
-            scrollSpeed: 1600,
-            scrollbars: false,
-            updateHash: false
-        });
+    $.scrollify({
+        section: ".content",
+        scrollSpeed: 1600,
+        scrollbars: false,
+        updateHash: false
+    });
 
+    var activePoint;
     $('.content').waypoint(function (direction) {
-        var activePoint = $(this.element);
-        if (direction === 'down') {　 //scroll down
+        activePoint = $(this.element);
+        var anmCount;
+        if (direction === 'down') {
             activePoint.find('.anm').css({
-                "opacity":"1"
-//                "transform":"translateY(0%)"
-            })
-        } else { //scroll up
+                "opacity": "1"
+            });
+            var sectionAnm = activePoint.find('.anm').attr('id');
+        } else {
             activePoint.find('.anm').css({
-                "opacity":"0",
+                "opacity": "0",
             });
         }
     }, {
-        offset: '70%'
+        offset: '10%'
     });
 
 });
