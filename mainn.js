36c018d4f5908f7764337a75fac7475900b4eef1
Eri Osawa <osawaeri@users.noreply.github.com>
2018-08-14 01:08:37 +0900

    37

--- mainn.js
+++ mainn.js
@@ -1,40 +1,26 @@
 $(function () {
 
-    $.scrollify({
-        section: ".content",
-        scrollSpeed: 1600,
-        scrollbars: false,
-        updateHash: false
-    });
-
-    $('#serious h1, #serious .h2').css({
-        "opacity": "0"
-    });
-    $(window).scroll(function () {
-        $('.content').each(function () {
-            var contentPos = $(this).offset().top;
-            var scroll = $(window).scrollTop();
-            var windowHeight = $(window).height();
-            if (scroll > contentPos - windowHeight) {
-                $("#serious h1, #serious .h2", this).css({
-                    "opacity": "1",
-                    "transform": "translateY(0%)"
-                });
-            } else {
-                $('#serious h1, #serious .h2', this).css({
-                    "opacity": "0"
-                });
-                $("#serious h1", this).css({
-                    "transform": "translteY(8%)"
-                });
-                $("#serious .h2", this).css({
-                    "transform": "translateY(16%)"
-                });
-            }
+        $.scrollify({
+            section: ".content",
+            scrollSpeed: 1600,
+            scrollbars: false,
+            updateHash: false
         });
 
+    $('.content').waypoint(function (direction) {
+        var activePoint = $(this.element);
+        if (direction === 'down') {　 //scroll down
+            activePoint.find('.anm').css({
+                "opacity":"1"
+//                "transform":"translateY(0%)"
+            })
+        } else { //scroll up
+            activePoint.find('.anm').css({
+                "opacity":"0",
+            });
+        }
+    }, {
+        offset: '70%'
     });
-
-
 
 });
