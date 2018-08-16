c1b4f56857601df8e7b26057bbae50edd259e97a
Eri Osawa <osawaeri@users.noreply.github.com>
2018-08-16 14:05:05 +0900

    47

--- mainn.js
+++ mainn.js
@@ -11,22 +11,52 @@ $(function () {
     var activePoint;
     var activeOp;
     var activeTrans;
+    var activeAnm;
+    var activeContent;
+    var anmBool = false;
+    var anmCount = 0;
     $('.content').waypoint(function (direction) {
         activePoint = $(this.element);
-        var anmCount;
         if (direction === 'down') {
             activeOp = activePoint.find('.op');
             activeTrans = activePoint.find('.trans');
+            activeAnm = activePoint.find('.anm');
+            activeContent = activePoint.prop('id');
             activeOp.css({
                 "opacity": "1",
             });
             activeTrans.css({
                 "transform": "translateY(0%)"
             });
+            if (activeContent === "course") {
+                anmBool = true;
+            } else {
+                anmBool = false;
+            }
         }
     }, {
         offset: '40%'
     });
 
+    if (anmBool === true) {
+        setInterval(function () {
+            courseClose();
+            anmCount++;
+            courseOpen();
+        }, 500);
+    }
+
+
+    function courseClose() {
+        $('h1.anm').css({
+            "display": "none"
+        });
+    }
+
+    function courseOpen() {
+        $('h1.anm').eq(anmCount).css({
+            "display": "inline-block"
+        });
+    }
 
 });
