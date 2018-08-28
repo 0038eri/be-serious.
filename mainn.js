eac2871a691ca24e0ee8327c5a4a71e447865444
Eri Osawa <osawaeri@users.noreply.github.com>
2018-08-28 18:51:51 +0900

    61

--- mainn.js
+++ mainn.js
@@ -12,20 +12,21 @@ $(function () {
     var activeOp;
     var activeTrans;
     var activeAnm;
-    var anmBool = false;
-    var anmCount = 0;
+    var bgAnm;
     $('.content').waypoint(function (direction) {
         activePoint = $(this.element);
         if (direction === 'down') {
             activeOp = activePoint.find('.op');
             activeTrans = activePoint.find('.trans');
             activeAnm = activePoint.find('.anm');
+            bgAnm = activePoint.find('.bgColor');
             activeOp.css({
                 "opacity": "1",
             });
             activeTrans.css({
                 "transform": "translateY(0%)"
             });
+            bgAnm.addClass("colorAnm")
         }
     }, {
         offset: '40%'
